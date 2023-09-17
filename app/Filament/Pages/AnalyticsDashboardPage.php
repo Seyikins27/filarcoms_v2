3d60fad99de33e44f41eb74cdaa6d4b936e04d3b
sheyi <seyiboyvvu@gmail.com>
2023-09-17 21:49:38 +0100

    Added Google Analytics Plugin by bezhansalleh

---
+++ app/Filament/Pages/AnalyticsDashboardPage.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Pages\Page;
+use BezhanSalleh\FilamentGoogleAnalytics\Widgets;
+
+class AnalyticsDashboardPage extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static string $view = 'filament.pages.analytics-dashboard-page';
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            Widgets\PageViewsWidget::class,
+            Widgets\VisitorsWidget::class,
+            Widgets\ActiveUsersOneDayWidget::class,
+            Widgets\ActiveUsersSevenDayWidget::class,
+            //Widgets\ActiveUsersFourteenDayWidget::class,
+            Widgets\ActiveUsersTwentyEightDayWidget::class,
+            Widgets\SessionsWidget::class,
+            Widgets\SessionsDurationWidget::class,
+            Widgets\SessionsByCountryWidget::class,
+            Widgets\SessionsByDeviceWidget::class,
+            Widgets\MostVisitedPagesWidget::class,
+            Widgets\TopReferrersListWidget::class,
+        ];
+    }
+}
