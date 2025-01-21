a20f1ffba8ed28388c7840189dfe6d9c848221d8
seyi <seyiboyvvu@gmail.com>
2025-01-21 20:30:03 +0100

    Added HasPageUrls Trait to Page Model

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -7,11 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Facades\Cache;
+use Z3d0X\FilamentFabricator\Models\Concerns\HandlesPageUrls;
 use Z3d0X\FilamentFabricator\Models\Contracts\Page as Contract;
 
 class Page extends Model implements Contract
 {
-    use HasFactory;
+    use HasFactory, HandlesPageUrls;
 
     public function __construct(array $attributes = [])
     {
