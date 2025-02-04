277ce954b6fc4f0e1820b107642ea5b8c1f617e1
seyi <seyiboyvvu@gmail.com>
2025-02-04 09:39:57 +0100

    Added User seeder and added can_publish column to user migration

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,7 +5,9 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
 
 class UserSeeder extends Seeder
 {
@@ -14,13 +16,23 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([
+        
+        $role=Role::create([
+            'name'=>'super_admin',
+            'guard_name'=>'web'
+        ]);
+        $user=User::create([
             'name'=>'Web Admin',
             'email'=>'admin@admin.com',
             'email_verified_at'=>date('Y-m-d h:i:s'),
             'password'=>Hash::make('password'),
-            'role_id'=>1,
+            //'role_id'=>1,
             'can_publish'=>1
+        ]);
+        DB::table('model_has_roles')->insert([
+            'role_id'=>$role->id,
+            'model_type'=>User::class,
+            'model_id'=>$user->id
         ]);
     }
 }
